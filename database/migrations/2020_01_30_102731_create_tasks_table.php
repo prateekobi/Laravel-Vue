bbee35ecc06138466611b18703600b776512de29
Prateek Obireddy <prateekobireddy@192-168-1-103.tpgi.com.au>
2020-01-30 22:04:13 +1100

    Last commit of the day - implemented default auth

--- database/migrations/2020_01_30_102731_create_tasks_table.php
+++ database/migrations/2020_01_30_102731_create_tasks_table.php
@@ -15,6 +15,8 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title');
+            $tabke->string('priority')->nullable();
             $table->timestamps();
         });
     }
